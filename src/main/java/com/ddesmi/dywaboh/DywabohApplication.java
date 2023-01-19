0017534779abbfb2e4f3965f93dec3e700c3a189
EmKJ <ekjacksondev@outlook.com>
2023-01-19 16:55:29 -0600

    scaffold and hello world

---
+++ src/main/java/com/ddesmi/dywaboh/DywabohApplication.java
@@ -0,0 +1,13 @@
+package com.ddesmi.dywaboh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DywabohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DywabohApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddesmi/dywaboh/DywabohApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddesmi.dywaboh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DywabohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
